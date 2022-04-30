27ca3e6365e4ad7ce2104997fa4c21ce61e00655
Vuk Topalovic <63306506+vukkt@users.noreply.github.com>
2022-04-30 16:38:52 +0200

    Update

---
+++ src/main/java/com/springboot/first/app/HelloWordController.java
@@ -0,0 +1,11 @@
+package com.springboot.first.app;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWordController {
+	// get
+	// post
+	// put
+	// delete
+}
