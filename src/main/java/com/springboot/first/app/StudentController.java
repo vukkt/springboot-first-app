a2401c3c22edbf21f004453b1f43ceccb56a8966
Vuk Topalovic <63306506+vukkt@users.noreply.github.com>
2022-04-30 17:21:54 +0200

    Update StudentController.java

--- src/main/java/com/springboot/first/app/StudentController.java
+++ src/main/java/com/springboot/first/app/StudentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -39,6 +40,11 @@ public class StudentController {
 	}
 
 	// building rest API to handle query parameters
-	// http://localhost:8080/student?firstName=Ramesh //?firstName=Ramesh is query
-	// parameter
+	// http://localhost:8080/student?firstName=Ramesh after (?) is query parameter
+	@GetMapping("/student/query")
+	public Student studentQueryParam(@RequestParam(name = "firstName") String firstName,
+			@RequestParam(name = "lastName") String lastName) {
+		return new Student(firstName, lastName);
+	}
+
 }
